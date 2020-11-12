675cbb49f0ebd5b5fb8413bede65d486fcb0a5df
Ioan Botez <85bioan@gmail.com>
2020-11-12 21:16:52 +0100

    updated drawer icons

--- src/Drawer.js
+++ src/Drawer.js
@@ -1,16 +1,15 @@
 import React from 'react'
 import { useHistory } from 'react-router-dom'
-import Drawer from '@material-ui/core/Drawer';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import List from '@material-ui/core/List';
-import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
+import Drawer from '@material-ui/core/Drawer'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import List from '@material-ui/core/List'
+import Typography from '@material-ui/core/Typography'
+import Divider from '@material-ui/core/Divider'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemIcon from '@material-ui/core/ListItemIcon'
+import ListItemText from '@material-ui/core/ListItemText'
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 
 const stringToUri = string => string.toLowerCase().replace(' ', '-')
 
@@ -39,7 +38,7 @@ const MyDrawer = ({classes}) => {
         <List>
           {['Welcome', 'Middle Ages', 'Nineteenth Century', 'Twentieth Century', 'Recent', 'Conclusion'].map((text, index) => (
             <ListItem button key={text} onClick={() => history.push(stringToUri(text))}>
-              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+              <ListItemIcon>{index > 0 && <ChevronRightIcon />}</ListItemIcon>
               <ListItemText primary={text} />
             </ListItem>
           ))}
