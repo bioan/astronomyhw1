1d116b8be9ad540af73c3720cd524a131777436a
Ioan Botez <85bioan@gmail.com>
2020-11-12 19:20:47 +0100

    added first image

--- src/periods/MiddleAges.js
+++ src/periods/MiddleAges.js
@@ -11,12 +11,14 @@ const MiddleAges = ({classes}) => {
 
       <Typography paragraph>
       1759 - The construction, led by Maximilian Hell, of the astronomical tower in Transilvania was complete(later destroyed by flames in 1798).
+      <br></br>
+      <img src='/MaximilianHell.jpg' />
       </Typography>
 
       <Typography paragraph>
       1762 - Ruggero Giuseppe Boscovich(one of the greatest astronomers of the time), made determinations of the latitudes and longitudes of Galaţi(now a city in Romania). Ruggero also supported various other astronomical studies in Galaţi.
       </Typography>
-      
+
       <Typography paragraph>
       1795 - Opening of the second observatory at Alba Iulia, by Bishop Ignatius Batthyany(later closed in 1860 for unknown reasons).
       </Typography>
