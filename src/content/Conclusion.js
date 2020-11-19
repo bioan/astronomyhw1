e8b977da7da29a19a7122f3b02000d9175f256ee
Alex Gabor <a.gabor.1@student.rug.nl>
2020-11-19 20:15:27 +0100

    intro+conc

--- src/content/Conclusion.js
+++ src/content/Conclusion.js
@@ -7,7 +7,7 @@ const Conclusion = ({classes}) => {
     <main className={classes.content}>
       <div className={classes.toolbar} />
       <Typography paragraph>
-        Conclusion
+        The conclusion that can be drawn from this article is that we cannot know exactly what were the main inspirations for the painting “The Starry Night”, but we can draw some “as close as possible” interpretations. The main interpretations of this painting can be found under the tab “Date and time”, which explains step by step why we think Vincent Van Gogh chose to paint the sky like that. Those conclusions are based purely on the letters sent between him and his brother, Theo van Gogh. 
       </Typography>
     </main>
   )
--- src/content/Introduction.js
+++ src/content/Introduction.js
@@ -7,7 +7,7 @@ const Introduction = ({classes}) => {
     <main className={classes.content}>
       <div className={classes.toolbar} />
       <Typography paragraph>
-        Introduction
+        We are 3 reporters that worked to build up a short summary of the life of Vincent Van Gogh, spotlighting ‘The Starry Night’. Vincent Van Gogh had a really interesting life, travelling in a lot of cities, taking insight from everything. He had a really close relationship with nature and the way it interacts with our world. He expressed his feelings through art and paintings, most of which were not known during his lifetime.
       </Typography>
     </main>
   )
