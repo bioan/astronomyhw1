da7ee864ae1995a9d97ea07ba48e6d54ab3f74bb
Alex Gabor <a.gabor.1@student.rug.nl>
2020-11-19 20:54:34 +0100

    ssw

--- src/content/References.js
+++ src/content/References.js
@@ -6,12 +6,12 @@ const References = ({classes}) => {
     <main className={classes.content}>
       <div className={classes.toolbar} />
       <Typography paragraph>
-          https://www.timeanddate.com/<br>
-		  https://www.vangoghroute.com/<br>
-		  http://vangoghletters.org/vg/<br>
-		  https://www.pri.org/stories/2015-01-29/was-van-goghs-starry-night-inspired-scientific-drawing<br>
-		https://www.theartnewspaper.com/blog/was-van-gogh-inspired-by-hokusai-s-great-wave-when-he-painted-his-starry-night<br>
-		https://www.artsy.net/news/artsy-editorial-russia-holding-vincent-van-goghs-sketch-starry-night-secret-storage-facility<br>
+          https://www.timeanddate.com/<br></br>
+		  https://www.vangoghroute.com/<br></br>
+		  http://vangoghletters.org/vg/<br></br>
+		  https://www.pri.org/stories/2015-01-29/was-van-goghs-starry-night-inspired-scientific-drawing<br></br>
+		https://www.theartnewspaper.com/blog/was-van-gogh-inspired-by-hokusai-s-great-wave-when-he-painted-his-starry-night<br></br>
+		https://www.artsy.net/news/artsy-editorial-russia-holding-vincent-van-goghs-sketch-starry-night-secret-storage-facility<br></br>
 		http://www.vggallery.com/drawings/p_1540.htm
       </Typography>
     </main>
