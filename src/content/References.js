b951ac593b6328472938b2683c4b289564923f41
Alex Gabor <a.gabor.1@student.rug.nl>
2020-11-19 20:40:59 +0100

    Update References.js

--- src/content/References.js
+++ src/content/References.js
@@ -6,7 +6,13 @@ const References = ({classes}) => {
     <main className={classes.content}>
       <div className={classes.toolbar} />
       <Typography paragraph>
-          References
+          https://www.timeanddate.com/<br>
+		  https://www.vangoghroute.com/<br>
+		  http://vangoghletters.org/vg/<br>
+		  https://www.pri.org/stories/2015-01-29/was-van-goghs-starry-night-inspired-scientific-drawing<br>
+		https://www.theartnewspaper.com/blog/was-van-gogh-inspired-by-hokusai-s-great-wave-when-he-painted-his-starry-night<br>
+		https://www.artsy.net/news/artsy-editorial-russia-holding-vincent-van-goghs-sketch-starry-night-secret-storage-facility<br>
+		http://www.vggallery.com/drawings/p_1540.htm
       </Typography>
     </main>
   )
